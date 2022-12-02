b1a093cef0eef4537bc8c49f6d487de6e9f641dc
lklk311 <lokendra0424.be20@chitkara.edu.in>
2022-12-02 10:02:54 +0530

    Basic-HTML

---
+++ Myscript.js
@@ -0,0 +1,39 @@
+function sum(n1,n2)
+{
+	res=n1+n2;
+	return res;
+}
+function sub(n1,n2)
+{
+	res=n1-n2
+	return res;
+}
+function multi(n1,n2)
+{
+	res=n1*n2;
+	return res;
+}
+function div(n1,n2)
+{
+	res=n1/n2;
+	return res;
+}
+function rem(n1,n2)
+{
+	res=n1%n2;
+	return res;
+}
+function use_of_array()
+{
+	var arr=["html","Java","Css","Jvascript","Php"];
+	//alert(arr.length);
+	for(i=0;i<arr.length;i++)
+	{
+	//document.write(arr[i]);
+	alert(arr[i]);
+	}
+}
+
+		
+
+		
